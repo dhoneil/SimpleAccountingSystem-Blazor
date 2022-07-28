8f4f7e2087c82706cc86b3fd93f11133d1b3f759
Dhoneil Angchangco <act.dangchangco@gmail.com>
2022-07-28 23:34:50 +0800

    changes

--- AccountingSystem/Client/Pages/ReceiveItemPage.razor.cs
+++ AccountingSystem/Client/Pages/ReceiveItemPage.razor.cs
@@ -20,6 +20,8 @@ namespace AccountingSystem.Client.Pages
         public List<Supplier> Suppliers { get; set; } = new();
         public List<Location> Locations { get; set; } = new();
         public Supplier CurrentSelectedSupplier { get; set; } = new();
+        public List<ReceivedItemDetail> ReceivedItemDetailList { get; set; } = new();
+        public ReceivedItemDetail CurrentReceivedItemDetail { get; set; } = new();
 
         protected override async Task OnInitializedAsync()
         {
@@ -45,10 +47,23 @@ namespace AccountingSystem.Client.Pages
 
         }
 
-        public async Task OnChangeSupplier(object selectedsupplier)
+        public async Task OnChangeSupplier(ChangeEventArgs selectedsupplier)
         {
-            var supplierid = (int)selectedsupplier;
-            CurrentSelectedSupplier = Suppliers.Where(s => s.SupplierId == supplierid)?.FirstOrDefault();
+            var selectedSuppId = selectedsupplier.Value.ToString();
+            CurrentSelectedSupplier = Suppliers.Where(s => s.SupplierId == int.Parse(selectedSuppId))?.FirstOrDefault();
+        }
+
+        public async Task AddToReceivedItemDetailList()
+        {
+            ReceivedItemDetailList.Add(CurrentReceivedItemDetail);
+            CurrentReceivedItemDetail = new();
+            StateHasChanged();
+        }
+
+        public async Task RemoveFromReceivedItemDetailList(int detailid)
+        {
+            var toremove = ReceivedItemDetailList.FirstOrDefault(s => s.ReceivedItemDetailId == detailid);
+            ReceivedItemDetailList.Remove(toremove);
         }
     }
 }
